b5427974531f365c7a7a1ca8608bd64651fb6df5
dan <danielkarongoi@gmail.com>
2025-03-10 05:42:17 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+// Function Declaration with Default Parameter
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function Expression with Default Parameter
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function Returning a Function (Closure)
+  function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  // Testing the functions
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("swim")); // "This Saturday, I want to swim!"
+  
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+  const encouragingPrompt = wrapAdjective("||");
+  console.log(encouragingPrompt("a hard worker")); // "You are ||a hard worker||!"
+  
